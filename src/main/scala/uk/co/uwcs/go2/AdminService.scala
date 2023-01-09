18054a78c747c70881e646e43097eb1c6341150d
Joey Harrison <joeyh021@icloud.com>
2023-01-09 14:14:54 +0000

    form submission good

--- src/main/scala/uk/co/uwcs/go2/AdminService.scala
+++ src/main/scala/uk/co/uwcs/go2/AdminService.scala
@@ -9,13 +9,7 @@ import org.http4s.twirl._
 import skunk._
 import skunk.implicits._
 import skunk.codec.all._
-
-import org.http4s.headers._
-import org.http4s.multipart.Multipart
-import org.http4s.server._
-import org.http4s.server.middleware.authentication.BasicAuth
-import org.http4s.server.middleware.authentication.BasicAuth.BasicAuthenticator
-import org.http4s.syntax.all._
+import org.http4s.headers.Location
 
 case class Redirect(source: String, sink: String, usages: Int)
 
@@ -26,9 +20,12 @@ class AdminService(val session: Resource[IO, Session[IO]]):
       case GET -> Root => getAllRedirects().map(html.panel.apply).flatMap(Ok(_))
       // case req @ POST -> Root => req.as[Redirect].flatMap(addNewRedirect) >> Found(Location(uri"/"))
       case req @ POST -> Root =>
-        req.decode { (m: UrlForm) =>
-          val s = m.values.mkString("\n")
-          Ok(s"Form Encoded Data\n$s")
+        req.decode { (f: UrlForm) =>
+          val src = f.values("source").headOption
+          val snk = f.values("sink").headOption
+          (src, snk) match
+            case (Some(src), Some(snk)) => addNewRedirect(src, snk) >> SeeOther(Location(uri"/"))
+            case _                      => BadRequest(s"Invalid data: " + f.values.mkString("\n"))
         }
     }
 
@@ -40,9 +37,8 @@ class AdminService(val session: Resource[IO, Session[IO]]):
 
     session.use(_.execute(getAllQ))
 
-  private def addNewRedirect(r: Redirect): IO[Unit] =
-    val addNewC: Command[Redirect] =
-      sql"INSERT INTO redirect_redirect (source, sink, usages, permanent) VALUES ($varchar, $varchar, $int4, 'false')".command
-        .gcontramap[Redirect]
+  private def addNewRedirect(source: String, sink: String): IO[Unit] =
+    val addNewC: Command[String ~ String] =
+      sql"INSERT INTO redirect_redirect (source, sink, usages, permanent) VALUES ($varchar, $varchar, 0, 'false')".command
 
-    session.use(_.prepare(addNewC).use(_.execute(r))).void
+    session.use(_.prepare(addNewC).use(_.execute(source, sink))).void
