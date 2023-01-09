1ee2f80e2ff675e24a567921ff38e26acfc6dd92
Joey Harrison <joeyh021@icloud.com>
2023-01-09 15:29:02 +0000

    update database schema to add date and time

--- src/main/scala/uk/co/uwcs/go2/AdminService.scala
+++ src/main/scala/uk/co/uwcs/go2/AdminService.scala
@@ -10,15 +10,21 @@ import skunk._
 import skunk.implicits._
 import skunk.codec.all._
 import org.http4s.headers.Location
+import java.time.OffsetDateTime
 
-case class Redirect(source: String, sink: String, usages: Int)
+case class Redirect(
+    source: String,
+    sink: String,
+    usages: Int,
+    used: Option[OffsetDateTime],
+    created: Option[OffsetDateTime]
+)
 
 class AdminService(val session: Resource[IO, Session[IO]]):
 
   val routes = HttpRoutes
     .of[IO] {
       case GET -> Root => getAllRedirects().map(html.panel.apply).flatMap(Ok(_))
-      // case req @ POST -> Root => req.as[Redirect].flatMap(addNewRedirect) >> Found(Location(uri"/"))
       case req @ POST -> Root =>
         req.decode { (f: UrlForm) =>
           val src = f.values("source").headOption
@@ -31,14 +37,14 @@ class AdminService(val session: Resource[IO, Session[IO]]):
 
   private def getAllRedirects() =
     val getAllQ: skunk.Query[Void, Redirect] =
-      sql"SELECT source, sink, usages FROM redirect_redirect"
-        .query(varchar(50) ~ varchar(1024) ~ int4)
+      sql"SELECT source, sink, usages, last_used, created FROM redirects"
+        .query(varchar(50) ~ varchar(1024) ~ int4 ~ timestamptz.opt ~ timestamptz.opt)
         .gmap[Redirect]
 
     session.use(_.execute(getAllQ))
 
   private def addNewRedirect(source: String, sink: String): IO[Unit] =
     val addNewC: Command[String ~ String] =
-      sql"INSERT INTO redirect_redirect (source, sink, usages, permanent) VALUES ($varchar, $varchar, 0, 'false')".command
+      sql"INSERT INTO redirects (source, sink) VALUES ($varchar, $varchar)".command
 
     session.use(_.prepare(addNewC).use(_.execute(source, sink))).void
--- src/main/scala/uk/co/uwcs/go2/RedirectService.scala
+++ src/main/scala/uk/co/uwcs/go2/RedirectService.scala
@@ -22,26 +22,23 @@ class RedirectService(val session: Resource[IO, Session[IO]]):
   private def redirect(source: String): IO[Response[IO]] =
     session
       .use { s => // use the database session
-        for {     // for comprehension to compose the effects in the IO monad
-          lookup <- s
-            .prepare(sinkQ)        // prepare the statement
-            .use(_.option(source)) // use statement to look up in db, ps.option returns either Some(row) or None
-
-          response <- lookup.map(Uri.fromString) match
-            case None          => NotFound()            // 404 if not found
-            case Some(Left(_)) => InternalServerError() // 500 if found but couldn't parse uri (unlikely)
-            case Some(Right(sink)) => TemporaryRedirect(Location(sink)) // 307 to redirect if found
-
-            // only increase count if we actually do the redirect
-            // could probably be nicer composed through some monad than using a condition
-          _ <- if response.status == TemporaryRedirect then s.prepare(countC).use(_.execute(source)) else IO.pure(())
-        } yield response
+        s.prepare(sinkQ).use(_.option(source)).map(_.map(Uri.fromString)).flatMap {
+          case None          => NotFound()            // 404 if not found
+          case Some(Left(_)) => InternalServerError() // 500 if found but couldn't parse uri (unlikely)
+          case Some(Right(sink)) =>
+            s.prepare(countC).use(_.execute(source))     // bump count
+              >> s.prepare(dateC).use(_.execute(source)) // bump last used date
+              >> MovedPermanently(Location(sink))        // 301 to redirect if found
+        }
       }
 
   // sink is varchar(1024)
   // source is varchar(50)
   private val sinkQ: skunk.Query[String, String] =
-    sql"""SELECT sink FROM redirect_redirect WHERE source=$varchar""".query(varchar(1024))
+    sql"SELECT sink FROM redirects WHERE source=$varchar".query(varchar(1024))
 
   private val countC: Command[String] =
-    sql"""UPDATE redirect_redirect SET usages = usages + 1 WHERE source=$varchar""".command
+    sql"UPDATE redirects SET usages = usages + 1 WHERE source=$varchar".command
+
+  private val dateC: Command[String] =
+    sql"UPDATE redirects SET last_used=now()::timestamptz WHERE source=$varchar".command
